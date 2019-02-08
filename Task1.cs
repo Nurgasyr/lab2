0845cc9ba3f273d5eac4c3c5967cb0836a639071
Nurgasyr <47244885+Nurgasyr@users.noreply.github.com>
2019-02-08 22:29:11 +0600

    Add files via upload

---
+++ Task1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task1
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader(@"C:\Users\NUR\Desktop\1.txt");
+            String s = sr.ReadToEnd();
+           // char[] arr = s.ToCharArray();
+            bool pl = true;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] != s[s.Length - 1 - i])
+                    pl = false;
+            }
+            if (pl == false)
+            {
+                Console.WriteLine("No");
+            }
+            else
+            {
+                Console.WriteLine("Yes");
+            }
+
+        }
+    }
+}
---
+++ Task2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace task2
+{
+    class Program
+    {   
+        public static bool prime(int a)
+        {   if (a == 1)
+                return false;
+            for(int i = 2; i < Math.Sqrt(a); i++)
+            {
+                if (a % i == 0)
+                { 
+                  return false;
+                }
+            }
+            return true;
+        }
+        static void Main(string[] args)
+        {
+          // FileStream f = new FileStream(@"C:\Users\NUR\source\repos\week2\task2\task2\bin\Debug\input.txt",FileMode.Open,FileAccess.Read);
+            StreamReader sr = new StreamReader("input.txt");
+            string s = sr.ReadToEnd();
+            StreamWriter sw = new StreamWriter("output.txt");
+            sr.Close();
+            string[] arr = s.Split();
+            for(int i = 0; i < arr.Length; i++)
+            {
+                if (prime(int.Parse(arr[i])) == true)
+                    sw.Write(arr[i] + " ");
+                    
+                }
+            sw.Close();
+        }
+    }
+}
