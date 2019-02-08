8b48e55dbbf0dac452daf5496b33c9cda760bed6
Nurgasyr <47244885+Nurgasyr@users.noreply.github.com>
2019-02-08 22:29:39 +0600

    Add files via upload

---
+++ task4.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace ConsoleApp1
+{
+    class task4
+    {
+        static void Main(string[] args)
+        {
+            string file = "input.txt";
+            string path = @"C:\Users\NUR\source\repos\ConsoleApp1\ConsoleApp1\path\" + file;
+            string path1 = @"C:\Users\NUR\source\repos\ConsoleApp1\ConsoleApp1\path1\" + file;
+            //creat
+            FileStream fs = new FileStream(@"C:\Users\NUR\source\repos\ConsoleApp1\ConsoleApp1\path\input.txt", FileMode.Create, FileAccess.Write);
+            //Copy
+            fs.Close();
+            File.Copy(path, path1);
+            //
+            File.Delete(@"C:\Users\NUR\source\repos\ConsoleApp1\ConsoleApp1\path\input.txt");
+        }
+    }
+}
